f94fc4895ad0399788ae017bf1e10e95187d35ab
ClovisVitor <clovisvitor.04@gmail.com>
2025-11-18 15:47:05 -0300

    Criação da rota PUT (Alterando Status)

--- backend/app.py
+++ backend/app.py
@@ -101,6 +101,49 @@ def get_tasks():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+# ROTA PUT → atualizar o campo "done" da task
+@app.put("/tasks/<int:task_id>")
+def update_task(task_id):
+    data = request.json
+    done = data.get("done")
+
+    # Validação
+    if done is None:
+        return jsonify({"error": "'done' é obrigatório e deve ser true ou false"}), 400
+
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+            UPDATE tasks
+            SET done = %s
+            WHERE id = %s
+            RETURNING id, title, done;
+        """, (done, task_id))
+
+        updated = cur.fetchone()
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        # Se não existir task com esse ID
+        if not updated:
+            return jsonify({"error": "Task não encontrada"}), 404
+
+        return jsonify({
+            "message": "Task atualizada com sucesso!",
+            "task": {
+                "id": updated[0],
+                "title": updated[1],
+                "done": updated[2]
+            }
+        }), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 # -------------------------------
 # Servidor Flask
